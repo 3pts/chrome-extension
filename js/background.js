3cdf10878d7a0c37ed2283f14551837fa85bfae3
Stefan Dimitrov <stefan@dimitrov.li>
2021-01-06 22:39:27 -0500

    Fix opening and closing extension.

--- js/background.js
+++ js/background.js
@@ -5,9 +5,15 @@
  */
 function browserActionListener(tab) {
   const targetUrl = tab.url;
-  const extensionView = `${chrome.runtime.getURL('html/view.html')}?host=${targetUrl}`;
-  
-  chrome.tabs.update(tab.id, {url: extensionView});
+  let extensionView = `${chrome.runtime.getURL('html/view.html')}?host=${targetUrl}`;
+
+  if (targetUrl.startsWith('chrome-extension://')) {
+    const viewSearch = new URL(targetUrl).searchParams;
+    extensionView = decodeURI(viewSearch.get('host'));
+  }
+  if (extensionView && extensionView !== 'null') {
+    chrome.tabs.update(tab.id, {url: extensionView});
+  }
 }
 
 chrome.browserAction.onClicked.addListener(browserActionListener);
